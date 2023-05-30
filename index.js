05166b95162fe6c8a74b6f3c798362ba5c2bec6f
Kristian Hannula <kristian.hannula@proton.me>
2023-05-30 20:47:17 +0300

    part 3.6 done

--- index.js
+++ index.js
@@ -66,6 +66,13 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    const nameExists = persons.some(person => person.name === body.name);
+    if (nameExists) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        });
+    }
+
     const person = {
         id: generateId(),
         name: body.name,
