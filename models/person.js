a21f783fab1021581b69129b1c6c6934836b2009
Kristian Hannula <kristian.hannula@proton.me>
2023-06-05 20:04:35 +0300

    Added lint and cleaned the backend, task 3.22 don

--- models/person.js
+++ models/person.js
@@ -7,40 +7,40 @@ const urlFiltered = process.env.MONGODB_URI_FILTERED
 
 console.log('connecting to', urlFiltered)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        const match = /^(\d{2}-\d{8}|\d{3}-\d{7})$/.test(v)
+        console.log(`Testing phone number ${v}, result: ${match}`)
+        return match
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        validate: {
-            validator: function (v) {
-                const match = /^(\d{2}-\d{8}|\d{3}-\d{7})$/.test(v);
-                console.log(`Testing phone number ${v}, result: ${match}`);
-                return match;
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        required: true,
-        minlength: 8
-    }
+    required: true,
+    minlength: 8
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
